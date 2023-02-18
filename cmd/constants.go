282c9a7629c591efadf0d417f42ad85c4f1d0af9
per <per@softteam.se>
2023-02-18 11:29:43 +0100

    Version 1.1.3 - Updated gorm

--- cmd/constants.go
+++ cmd/constants.go
@@ -4,4 +4,4 @@ const constDateLayout = "2006-01-02"
 const listStoreColumnInvoiceNumber = 0
 
 const applicationName = "SoftInvoice"
-const applicationVersion = "1.1.2"
+const applicationVersion = "1.1.3"
